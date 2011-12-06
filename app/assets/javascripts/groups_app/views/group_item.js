6ea560e9f61d5760ac97f5cab947e374ac67e988
bobz <bobz@bobz.in>
2011-12-06 06:43:56 -0500

    sorta working

--- app/assets/javascripts/groups_app/views/group_item.js
+++ app/assets/javascripts/groups_app/views/group_item.js
@@ -14,14 +14,19 @@ GroupsApp.Views.GroupItem = Backbone.View.extend({
   renderFormContents: function() {
     this.$('.group-name').text(this.model.escape('name'));
 
-    this.$('.group-link').attr("href", this.groupUrl());
-
     this.$('.group-members').text("");
     var self = this;
     this.model.users.each( function(user) {
       self.$('.group-members').append(user.escape('email'));
     });
-      
+
+    if (this.model.owner.email == GroupApp.current_user)
+    {
+      var edit_link = document.createElement('a');
+      $(edit_link).text("Edit");
+      $(edit_link).attr("href", this.groupUrl());
+      this.$('.group-actions').append( edit_link );
+    }  
   },
 
   groupUrl: function() {
