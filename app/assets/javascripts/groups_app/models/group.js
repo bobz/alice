fbb402209a1ab3974f6d3cc82eaabb8ef56e676b
bobz <bobz@bobz.in>
2011-12-07 23:13:43 -0500

    some work

--- app/assets/javascripts/groups_app/models/group.js
+++ app/assets/javascripts/groups_app/models/group.js
@@ -1,10 +1,8 @@
 GroupsApp.Models.Group = Backbone.Model.extend({
   
   initialize: function() {
-    if (this.has('owner')) {
-      var owner = new GroupsApp.Models.User(this.get('owner'));
-      this.setOwner(owner);
-    }
+    var owner = new GroupsApp.Models.User(this.get('owner'));
+    this.setOwner(owner);
     var users = new GroupsApp.Collections.Users;
     users.reset(this.get('users'));
     this.setUsers(users);
@@ -13,7 +11,7 @@ GroupsApp.Models.Group = Backbone.Model.extend({
 
   schema: {
     name: { type: 'Text' },
-    owner: { type: 'Select', options: ['Option 1', 'Option 2']}
+    owner: { type: 'Select', options: this.users }
   },
 
   urlRoot: '/groups',
