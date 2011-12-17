90a2a69cd0b0b1f8130a214c556866f3e665a593
bobz <bobz@bobz.in>
2011-12-17 14:14:35 -0500

    controllers

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,83 +1,27 @@
 class ClaimsController < ApplicationController
-  # GET /claims
-  # GET /claims.json
-  def index
-    @claims = Claim.all
+  before_filter :authorize
+  respond_to :html, :json
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @claims }
+  def index
+    if (params[:line_item])
+      @claims = LineItem.find(params[:line_item]).claims
+    else
+      @claims = Claim.all
     end
+    respond_with(@claims)
   end
 
-  # GET /claims/1
-  # GET /claims/1.json
   def show
     @claim = Claim.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @claim }
-    end
   end
 
-  # GET /claims/new
-  # GET /claims/new.json
-  def new
-    @claim = Claim.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @claim }
-    end
-  end
-
-  # GET /claims/1/edit
-  def edit
-    @claim = Claim.find(params[:id])
-  end
-
-  # POST /claims
-  # POST /claims.json
   def create
-    @claim = Claim.new(params[:claim])
-
-    respond_to do |format|
-      if @claim.save
-        format.html { redirect_to @claim, notice: 'Claim was successfully created.' }
-        format.json { render json: @claim, status: :created, location: @claim }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @claim.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with(Claim.create(params[:claim]))
   end
 
-  # PUT /claims/1
-  # PUT /claims/1.json
   def update
-    @claim = Claim.find(params[:id])
-
-    respond_to do |format|
-      if @claim.update_attributes(params[:claim])
-        format.html { redirect_to @claim, notice: 'Claim was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @claim.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /claims/1
-  # DELETE /claims/1.json
-  def destroy
-    @claim = Claim.find(params[:id])
-    @claim.destroy
-
-    respond_to do |format|
-      format.html { redirect_to claims_url }
-      format.json { head :ok }
-    end
+    claim = Claim.find(params[:id])
+    claim.update_attributes(params[:claim])
+    respond_with(claim)
   end
 end
