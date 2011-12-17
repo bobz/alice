2ac4bdced3a1cd16455b5a62e3bf34e46e7a584d
bobz <bobz@bobz.in>
2011-12-17 08:46:04 -0500

    change hidden default to boolean

--- db/migrate/20111214091205_add_hidden_to_groups.rb
+++ db/migrate/20111214091205_add_hidden_to_groups.rb
@@ -1,6 +1,6 @@
 class AddHiddenToGroups < ActiveRecord::Migration
   def change
-    add_column :groups, :hidden, :boolean, :default => 0
+    add_column :groups, :hidden, :boolean, :default => false
     add_column :users, :id_group_id, :integer 
   end
 end
